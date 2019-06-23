209b790937a1b14caab72179def0b7792af6daa5
Georg Förster <georg.foerster@awo.org>
2019-06-23 23:06:59 +0200

    Daten laden / Notebook erstellt

--- rscripts/test.r
+++ rscripts/test.r
@@ -7,36 +7,31 @@ library(nlme)
 ## Bei Bedarf Daten runterladen
 
 
-df <- tibble()
-
-
-for (jahr in 2011:2018) {
-
-  con <- sprintf("https://www.statistik-berlin-brandenburg.de/opendata/EWR_Ortsteile_%i-12-31.csv", jahr) %>% url()
+if (file.exists("data.raw/pop.ortsteile.RDS")) {
   
-  df.frag <- try(read_csv2(con, locale = locale(encoding = "latin1")), silent = TRUE)  
+  df <- readRDS("data.raw/pop.ortsteile.RDS")
+
+} else {
+
+  df <- tibble()
+  
+  for (jahr in 2011:2018) {
+  
+    con <- sprintf("https://www.statistik-berlin-brandenburg.de/opendata/EWR_Ortsteile_%i-12-31.csv", jahr) %>% url()
     
-  if (!("try-error" %in% class(df.frag))) {
-    df.frag$jahr = jahr
-    df <- rbind(df, df.frag)
+    df.frag <- try(read_csv2(con, locale = locale(encoding = "latin1")), silent = TRUE)  
+      
+    if (!("try-error" %in% class(df.frag))) {
+      df.frag$jahr = jahr
+      df <- rbind(df, df.frag)
+    }
+    
+  
   }
   
+  df <- df %>% 
+    rename(bezirk = `Bez-Name`, ortsteil = `Ortst-Name`)
+  
+  saveRDS(df, "data.raw/pop.ortsteile.RDS")
 
 }
-
-
-df <- df %>% 
-  rename(bezirk = `Bez-Name`, ortsteil = `Ortst-Name`)
-
-
-
-df %>% 
-  group_by(jahr, ortsteil) %>% 
-  summarize(pop = sum(Häufigkeit)) %>% 
-  lmList(object = pop ~ I(jahr - 2018) | ortsteil)
-
-  #filter(ortsteil == "Spandau") %>% 
-  group_by(jahr, Altersgr) %>% summarize(pop = sum(Häufigkeit)) %>% 
-  ggplot(aes(x = Altersgr, y = jahr, alpha = pop)) +
-  geom_tile() +
-  theme_minimal()
